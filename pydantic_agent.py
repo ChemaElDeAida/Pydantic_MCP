d64d0d88cebd634430b4dd87d128601cbe25c1f1
ChemaElDeAida <123168609+ChemaElDeAida@users.noreply.github.com>
2025-04-24 16:10:53 +0200

    Cambios menos a pydantic_agent

--- pydantic_agent.py
+++ pydantic_agent.py
@@ -5,26 +5,34 @@ from pydantic_ai import Agent
 
 import asyncio
 
-#Imports para Gemini
+#Imports y creacción del modelo para Gemini
 from pydantic_ai.models.gemini import GeminiModel
 from pydantic_ai.providers.google_gla import GoogleGLAProvider
 gemini_model = GeminiModel(
     'gemini-2.0-flash', provider=GoogleGLAProvider(api_key='AIzaSyAYXNByPMyooQTZDig21a088vAus0bnV0I')
 )
 
-#Imports para Ollama
+
+
+#Imports y creacción del modelo para Ollama
 from pydantic_ai.models.openai import OpenAIModel
 from pydantic_ai.providers.openai import OpenAIProvider
 ollama_model = OpenAIModel(
     model_name='qwen2.5', provider=OpenAIProvider(base_url='http://localhost:11434/v1')
 )
 
+
+
+#Creacción del cliente y el agente, usa las tools ofrecidas en el .json especificado
+# y el agente está creado con el modelo especificado.
 async def get_pydantic_ai_agent():
     client = mcp_client.MCPClient()
     client.load_servers("GhidraMCP.json")
     tools = await client.start()
     return client, Agent(model=ollama_model, tools=tools)
 
+
+#Creacción del main para correr el agente y crear una pequeña interfaz sencilla para el chat 
 async def main():
     client, agent = await get_pydantic_ai_agent()
     while True:
@@ -36,7 +44,7 @@ async def main():
             break
         
         result = await agent.run(user_input)
-        print('['+agent.model.model_name+']:', result.output)
+        print('['+ollama_model.model_name+']:', result.output)
 
 if __name__ == '__main__':
     asyncio.run(main())
